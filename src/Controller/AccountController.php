336646cd407d5f678652f76e26189a649c531369
AndyDurbecq <andy.durbecq@gmail.com>
2018-11-04 20:36:56 +0100

    Section 7

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Entity\PasswordUpdate;
+use App\Form\RegistrationType;
+use App\Form\PasswordUpdateType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AccountController extends AbstractController
+{
+    /**
+     * Permet d'afficher et de gérer le formulaire de connexion
+     * 
+     * @Route("/login", name="account_login")
+     * 
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+
+        $username = $utils->getLastUsername();
+
+        return $this->render('account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * Permet de se déconnecter
+     * 
+     * @Route("/logout", name="account_logout")
+     */
+    public function logout()
+    {
+        // ...
+    }
+
+    /**
+     * Permet d'afficher le form d'inscription
+     * 
+     * @Route("/register", name="account_register")
+     * 
+     * @return Response
+     */
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder) 
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getHash());
+            
+            $user->setHash($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success', 
+                "Votre compte à bien été créé ! Vous pouvez maintenant vous connecter !"
+            );
+
+            return $this->redirectToRoute('account_login');
+        }
+
+        return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);    
+    }
+
+    /**
+     * Permet de traiter le profile
+     *
+     * @Route("/account/profile", name="account_profile")
+     * 
+     * @return Response
+     */
+    public function profile(Request $request, Objectmanager $manager) 
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {            
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash('success', 'Modifications faites !');            
+        }
+
+        return $this->render('account/profile.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Modifie le mot de passe
+     *
+     * @Route("account/password-update", name="account_password")
+     * 
+     * @return response
+     */
+    public function uppdatePassword(Request $request, UserPasswordEncoderInterface $encoder, Objectmanager $manager) 
+    {
+        $passwordUpdate = new PasswordUpdate();
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(PasswordUpdateType::class, $passwordUpdate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 1. Vérifier que le old = pass du user
+            if (!password_verify($passwordUpdate->getOldPassword(), $user->getHash())){
+                // Gérer l'erreur
+                $form->get('oldPassword')->addError(new FormError("Ancien mot de passe non valide !"));
+            } else {
+                $hash = $encoder->encodePassword($user, $passwordUpdate->getNewPassword());
+
+                $user->setHash($hash);
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash('success', 'Mot de passe modifié avec succès !');
+                
+                return $this->redirectToRoute('homepage');
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView()   
+        ]);
+    }
+}
